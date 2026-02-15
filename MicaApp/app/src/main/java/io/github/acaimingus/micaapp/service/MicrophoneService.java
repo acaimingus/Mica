a8675c2044cca1f05e563daa2b477f3b2902136e
Adam Martula <martulaadam@gmail.com>
2026-02-15 10:13:51 +0100

    Add the type for the service start for Android versions that need it

--- MicaApp/app/src/main/java/io/github/acaimingus/micaapp/service/MicrophoneService.java
+++ MicaApp/app/src/main/java/io/github/acaimingus/micaapp/service/MicrophoneService.java
@@ -7,8 +7,10 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ServiceInfo;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import androidx.annotation.Nullable;
@@ -72,7 +74,11 @@ public class MicrophoneService extends Service implements IAudioDataListener {
                 .setOngoing(true)
                 .build();
 
-        startForeground(1, notification);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+            startForeground(1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE);
+        } else {
+            startForeground(1, notification);
+        }
 
         // START_STICKY = Restart service if killed by OS
         recordingController.startRecording();
