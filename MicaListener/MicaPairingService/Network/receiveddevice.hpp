22853e55630d2b18f20d36d0f89e27eaf066b8e8
Adam Martula <martulaadam@gmail.com>
2026-09-07 22:05:11 +0200

    Add receiveddevice header comment

--- MicaListener/MicaPairingService/Network/receiveddevice.hpp
+++ MicaListener/MicaPairingService/Network/receiveddevice.hpp
@@ -1,16 +1,31 @@
+/*
+ * Copyright (c) 2026 Adam Martula
+ * This source code is licensed under the MIT license found in the LICENSE file in the root of this source tree.
+ *
+ * Description: Data Transfer Object (DTO) representing a discovered network device.
+ *              This class remains header-only because it is essentially a lightweight DTO
+ *              with trivial accessors, which enables compiler inlining without compilation unit overhead.
+ */
+
 #pragma once
+
 #include <string>
 #include <utility>
 
 namespace MicaPairingService::Network
 {
+    /// @brief Lightweight Data Transfer Object (DTO) for discovered network devices
     class ReceivedDevice
     {
     public:
-        ReceivedDevice(std::string _ip, const int _port, std::string _deviceName) : deviceName(std::move(_deviceName)), deviceIp(std::move(_ip)), devicePort(_port)
+        /// @brief Constructor for ReceivedDevice
+        /// @param _ip IP address of the device
+        /// @param _port Port of the device
+        /// @param _deviceName Name of the device
+        ReceivedDevice(std::string _ip, const int _port, std::string _deviceName)
+            : deviceName(std::move(_deviceName)), deviceIp(std::move(_ip)), devicePort(_port)
         {
-            // Empty
-        };
+        }
 
         /// @brief Public getter for the IP variable
         [[nodiscard]] std::string GetIp() const
