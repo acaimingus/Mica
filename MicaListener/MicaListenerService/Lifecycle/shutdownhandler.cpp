2d17fd61707771ff9515ec8dc7eee375babd8c33
Adam Martula <martulaadam@gmail.com>
2026-09-07 22:41:59 +0200

    Add header comments

--- MicaListener/MicaListenerService/Lifecycle/shutdownhandler.cpp
+++ MicaListener/MicaListenerService/Lifecycle/shutdownhandler.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2026 Adam Martula
+ * This source code is licensed under the MIT license found in the LICENSE file in the root of this source tree.
+ *
+ * Description: Implementation of ShutdownHandler for gentle shutdown of the listener service.
+ */
+
 #include "shutdownhandler.hpp"
 
 namespace MicaListener::MicaListenerService::Lifecycle
--- MicaListener/MicaListenerService/Notification/notificationmanager.hpp
+++ MicaListener/MicaListenerService/Notification/notificationmanager.hpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2026 Adam Martula
+ * This source code is licensed under the MIT license found in the LICENSE file in the root of this source tree.
+ *
+ * Description: Notification manager class for handling desktop notifications via libnotify.
+ */
+
 #pragma once
 
 #include <string_view>
